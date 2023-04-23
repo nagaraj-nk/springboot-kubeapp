a42a04d2e697ffe47b0f0aa84a24f9d27104562a
Nagaraj N <nagaraj.srjons@gmail.com>
2023-04-23 17:10:02 +0530

    initial commit

---
+++ src/test/java/com/srjons/kubeapp/KubeappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srjons.kubeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
